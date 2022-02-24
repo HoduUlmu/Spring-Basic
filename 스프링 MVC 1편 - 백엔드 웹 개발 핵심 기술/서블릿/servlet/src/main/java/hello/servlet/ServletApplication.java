32d2c928134fdc5ef8d562e3472049909f8505e4
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-02-24 19:11:48 +0900

    Spring MVC BASIC

--- 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/서블릿/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/서블릿/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 스프링이 자동으로 내 패키지 하위 패키지까지 뒤져서 서블릿 등록
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views", ".jsp");
+//	}
 }
