6fee453dea516c9a69e3f44491ef57da9e059d52
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-02-26 04:15:56 +0900

    JWT Tutorial

---
+++ Spring Boot JWT Tutorial/src/main/java/tutorial/jwt/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package tutorial.jwt.repository;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import tutorial.jwt.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
